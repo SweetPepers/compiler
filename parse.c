401d91bad4fbf133ebe96a205724878b3df11707
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-09-09 17:28:34 +0800

    [83] 支持 % 和 %=

--- parse.c
+++ parse.c
@@ -70,11 +70,11 @@ static Obj *CurrentFn;
 // exprStmt = expr? ";"
 // expr = assign ("," expr)?
 // assign = equality (assignOp assign)?
-// assignOp = "=" | "+=" | "-=" | "*=" | "/="
+// assignOp = "=" | "+=" | "-=" | "*=" | "/=" | "%="
 // equality = relational ("==" relational | "!=" relational)*
 // relational = add ("<" add | "<=" add | ">" add | ">=" add)*
 // add = mul ("+" mul | "-" mul)*
-// mul = cast ("*" cast | "/" cast)*
+// mul = cast ("*" cast | "/" cast | "%" cast)*
 // cast = "(" typeName ")" cast | unary
 // unary = ("+" | "-" | "*" | "&" | "!" | "~") cast
 //       | ("++" | "--") unary
@@ -834,7 +834,7 @@ static Node *toAssign(Node *Binary) {
 
 // 解析赋值
 // assign = equality (assignOp assign)?
-// assignOp = "=" | "+=" | "-=" | "*=" | "/="
+// assignOp = "=" | "+=" | "-=" | "*=" | "/=" | "%="
 static Node *assign(Token **Rest, Token *Tok) {
   // equality
   Node *Nd = equality(&Tok, Tok);
@@ -859,6 +859,10 @@ static Node *assign(Token **Rest, Token *Tok) {
   // ("/=" assign)?
   if (equal(Tok, "/="))
     return toAssign(newBinary(ND_DIV, Nd, assign(Rest, Tok->Next), Tok));
+
+  // ("%=" assign)?
+  if (equal(Tok, "%="))
+    return toAssign(newBinary(ND_MOD, Nd, assign(Rest, Tok->Next), Tok));
 
   *Rest = Tok;
   return Nd;
@@ -1020,12 +1024,12 @@ static Node *add(Token **Rest, Token *Tok) {
 }
 
 // 解析乘除
-// mul = cast ("*" cast | "/" cast)*
+// mul = cast ("*" cast | "/" cast | "%" cast)*
 static Node *mul(Token **Rest, Token *Tok) {
   // cast
   Node *Nd = cast(&Tok, Tok);
 
-  // ("*" cast | "/" cast)*
+  // ("*" cast | "/" cast | "%" cast)*
   while (true) {
     Token *Start = Tok;
 
@@ -1038,6 +1042,12 @@ static Node *mul(Token **Rest, Token *Tok) {
     // "/" cast
     if (equal(Tok, "/")) {
       Nd = newBinary(ND_DIV, Nd, cast(&Tok, Tok->Next), Start);
+      continue;
+    }
+
+    // "%" cast
+    if (equal(Tok, "%")) {
+      Nd = newBinary(ND_MOD, Nd, cast(&Tok, Tok->Next), Start);
       continue;
     }
 
